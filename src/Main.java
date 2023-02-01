ded1321d247f72120b6e19e210d3035efd57590e
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-02-01 11:47:07 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-       int ticketPrice = 1835 ; // стоимость билета
-        int bonusStep = 20 ; // шаг начисления бонусов
-        int bonusMiles = ticketPrice / bonusStep ; // начисляется бонусных миль
-        System.out.println("Вам начислено " + bonusMiles + " миль ") ;
+        int ticketPrice = 1835; // стоимость билета
+        int bonusStep = 20; // шаг начисления бонусов
+        int bonusMiles = ticketPrice / bonusStep; // начисляется бонусных миль
+        System.out.println("Вам начислено " + bonusMiles + " миль ");
 
     }
 }
