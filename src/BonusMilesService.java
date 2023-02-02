cc0edb91a7ce58680f02e3184ecc6fcb5747d142
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-02-02 15:16:17 +0300

    Calculation upgrade

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int resultMiles = cost / 20;
+
+        return resultMiles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketPrice = 1835; // стоимость билета
-        int bonusStep = 20; // шаг начисления бонусов
-        int bonusMiles = ticketPrice / bonusStep; // начисляется бонусных миль
-        System.out.println("Вам начислено " + bonusMiles + " миль ");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
